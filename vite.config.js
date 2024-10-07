e4f3efcd2b1dcf40848c2805510fe5c6da89c911
MTR-BRAND <mahmudaarafat0077@gmail.com>
2024-10-07 23:04:23 +0600

    first commit

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,15 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-
-// If you need to inject CSS, you can include this plugin
 import cssInjectedByJsPlugin from 'vite-plugin-css-injected-by-js';
 
 export default defineConfig({
   plugins: [
     react(),
-    cssInjectedByJsPlugin() // Add this plugin if Bootstrap's CSS isn't loading correctly
+    cssInjectedByJsPlugin()
   ],
   build: {
     rollupOptions: {
-      external: ['bootstrap'] // Explicitly externalize Bootstrap
+      external: ['bootstrap']
     }
   }
 });
