9d4ca53b7e78643141a31778de723d87966d5dd5
MTR-BRAND <mahmudaarafat0077@gmail.com>
2024-10-08 00:14:37 +0600

    Fix bootstrap import

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import bootstrap from 'vite-plugin-bootstrap';
 
 export default defineConfig({
-  plugins: [react(), bootstrap()],
+    plugins: [react()],
+    // any other configurations
 });
