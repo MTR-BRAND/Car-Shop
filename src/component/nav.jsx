f25249eec0c234db14150f26e7a4aee9fc63ba7c
MTR-BRAND <mahmudaarafat0077@gmail.com>
2024-10-07 23:54:34 +0600

    Fix bootstrap import

--- src/component/nav.jsx
+++ src/component/nav.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
 import './component.css'; // Import custom CSS
 import { Navbar, Nav, Container } from 'react-bootstrap'; // Using react-bootstrap for the Navbar
 
--- vite.config.js
+++ vite.config.js
@@ -3,14 +3,14 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      external: ['bootstrap/dist/css/bootstrap.min.css']
+    }
+  },
   resolve: {
     alias: {
       // Add aliases if needed
-    },
-  },
-  build: {
-    rollupOptions: {
-      external: ['bootstrap/dist/css/bootstrap.min.css'], // add this line
     },
   },
 });
