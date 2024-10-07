3bfdd9041f6061cf9231ec7d2925c74202c00a40
MTR-BRAND <mahmudaarafat0077@gmail.com>
2024-10-07 23:22:15 +0600

    Fix bootstrap import

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ export default defineConfig({
   plugins: [react()],
   build: {
     rollupOptions: {
-      external: ['bootstrap']
-    }
-  }
+      external: ['bootstrap/dist/css/bootstrap.min.css'], // Add this line
+    },
+  },
 });
